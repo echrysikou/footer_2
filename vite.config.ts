c8a492ab06044c5656456c23f4735204be5729ef
Elena Chrysikou <116569141+ElenaChrysikou@users.noreply.github.com>
2025-10-31 12:49:34 +0200

    EUUB-329: Basic footer react component

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+import typescript from "@rollup/plugin-typescript";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), typescript()],
+  define: {
+    'process.env.NODE_ENV': JSON.stringify('production'),
+    'process.env': {}
+  },
+  build: {
+    lib: {
+      formats: ["es", "umd"],
+      entry: resolve(__dirname, 'src/web-component.tsx'),
+      name: 'urbreath-footer',
+      fileName: (format) => `urbreath-footer.${format}.js`,
+    },
+    rollupOptions: {
+      output: {
+        globals: {
+        },
+      },
+    },
+  },
+});
